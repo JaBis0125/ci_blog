5aea0b00811f5015e72cfe81b0a235659a0e9549
JaBis0125 <156205391+JaBis0125@users.noreply.github.com>
2024-04-03 02:47:49 +0900

    Add files via upload

---
+++ White/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from './Compnonet/Navbar'
+import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import  Home  from './Compnonet/Home';
+import About from './Compnonet/About';
+import Blog from './Compnonet/Blog';
+import Bloginfo from './Compnonet/Bloginfo';
+ import Footer from './Compnonet/Footer';
+export default function App() {
+  return (
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:id" element={<Bloginfo />} />
+
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+  )
+}
